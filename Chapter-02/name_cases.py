963e6132f5c8c6e6000ccd5be730197d77896e9d
SFish860 <sagemfisher@gmail.com>
2024-02-08 10:41:51 -0700

    2nd commit

---
+++ Chapter-02/name_cases.py
@@ -0,0 +1,14 @@
+message = "Hello, Eric would you like to learn some Python today?"
+print(message)
+
+name = "john"
+message = "how are you?"
+full_message = ("Hello, "+ name.title() + " " + message)
+
+print(full_message)
+
+author =  " \tfranklin d. roosevelt "
+quote = '\n\t"The only thing we have to fear is fear itself."'
+message = (author.strip().title() + " once said, " + quote)
+
+print(message)
